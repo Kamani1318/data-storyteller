26de96cf32d954f56fb9e12a5441b7f6ee7ee639
Aryan Kamani <aryankamani@gmail.com>
2023-07-28 17:10:15 +0530

    app.py changed

--- app.py
+++ app.py
@@ -5,26 +5,27 @@ from PIL import  Image
 
 # Custom imports 
 from multipage import MultiPage
-from pages import data_upload, machine_learning, metadata, data_visualize, redundant # import your pages here
+from pages import data_upload, machine_learning, metadata, data_visualize, redundant, Final_Preprocessed_Data # import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
 
 # Title of the main page
-display = Image.open('Logo.png')
+display = Image.open('/Users/aryankamani/Data-Analysis-Tool/Machine Learning in Healthcare.png')
 display = np.array(display)
-# st.image(display, width = 400)
-# st.title("Data Storyteller Application")
-col1, col2 = st.beta_columns(2)
-col1.image(display, width = 400)
-col2.title("Data Storyteller Application")
+col1, col2 = st.columns(2)
+col1.image(display, width = 250)
+col2.title("Data Preprocessing Application")
 
 # Add all your application here
 app.add_page("Upload Data", data_upload.app)
 app.add_page("Change Metadata", metadata.app)
+app.add_page("Final Preprocessed Data",Final_Preprocessed_Data.app )
+app.add_page("Data Visualisation",data_visualize.app)
 app.add_page("Machine Learning", machine_learning.app)
-app.add_page("Data Analysis",data_visualize.app)
 app.add_page("Y-Parameter Optimization",redundant.app)
+
 
 # The main app
 app.run()
+
